3474d0657d0021e8fd906e6ad889358d01ea1f89
dbolgh <danillo.bolgheroni@gmail.com>
2023-11-15 14:28:45 -0300

    adjusted web.config file

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = dash.Dash(__name__)
 
 app.layout = html.Div("Hello, Dash in Docker!")
 
-# if __name__ == '__main__':
-#     app.run_server(debug=True, host='0.0.0.0', port=8050)
+if __name__ == '__main__':
+    app.run_server(debug=True)
