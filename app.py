036e5731a0b1972d4741c9cf76b492926186b110
dbolgh <danillo.bolgheroni@gmail.com>
2023-11-15 13:19:52 -0300

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+import dash
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div("Hello, Dash in Docker!")
+
+if __name__ == '__main__':
+    app.run_server(debug=True, host='0.0.0.0', port=8050)
